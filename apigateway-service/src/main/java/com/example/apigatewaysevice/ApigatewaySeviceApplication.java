9aec404df8a9bb3bdf5bf58843a2dfaa9a8f6ac0
“jeonga-Son” <jjeong0106@gmail.com>
2023-03-31 11:16:56 +0900

    initial commit

---
+++ apigateway-service/src/main/java/com/example/apigatewaysevice/ApigatewaySeviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigatewaysevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewaySeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewaySeviceApplication.class, args);
+	}
+
+}
