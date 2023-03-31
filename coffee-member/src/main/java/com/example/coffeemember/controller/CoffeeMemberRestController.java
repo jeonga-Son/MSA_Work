4732c75273a21dee70748243dadc6ba7f98a682c
“jeonga-Son” <jjeong0106@gmail.com>
2023-03-31 12:11:15 +0900

    [feat] update CoffeeMemberRestController

--- coffee-member/src/main/java/com/example/coffeemember/controller/CoffeeMemberRestController.java
+++ coffee-member/src/main/java/com/example/coffeemember/controller/CoffeeMemberRestController.java
@@ -1,14 +1,44 @@
 package com.example.coffeemember.controller;
 
+import com.example.coffeemember.service.MemberService;
+import com.example.coffeemember.vo.MemberVO;
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/coffee-member")
 public class CoffeeMemberRestController {
+    @Autowired
+    MemberService memberService;
+
     @GetMapping("/")
     public String coffeeMember() {
         return "Coffee Member";
+    }
+
+    @GetMapping("/existMember/{memberName}")
+    public boolean existMember(@PathVariable("memberName") String memberName) {
+        MemberVO vo = new MemberVO();
+        vo.setMemberName(memberName);
+
+        if(memberService.findMember(vo) != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @GetMapping("/createMemberTable")
+    public void createMemberTable() {
+        memberService.createMemberTable();
+    }
+
+    @GetMapping("/insertMember")
+    public void insertMember() {
+        memberService.insertMember();
     }
 }
