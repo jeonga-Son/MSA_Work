575bd3197a271fb27e7d2b298244be961a9ed100
“jeonga-Son” <jjeong0106@gmail.com>
2023-03-31 11:20:06 +0900

    [rename] update CoffeeStatusRestController

--- coffee-status/src/main/java/com/example/coffeestatus/controller/CoffeeStatusRestController.java
+++ coffee-status/src/main/java/com/example/coffeestatus/controller/CoffeeStatusRestController.java
@@ -1,12 +1,14 @@
 package com.example.coffeestatus.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/coffee-status")
 public class CoffeeStatusRestController {
-    @GetMapping("/status")
-    public String hello() {
+    @GetMapping("/")
+    public String coffeeStatus() {
         return "Coffee status";
     }
 }
