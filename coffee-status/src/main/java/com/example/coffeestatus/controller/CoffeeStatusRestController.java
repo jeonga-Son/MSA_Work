7a315c127b2426bd01cc4ece4d50f8d363532b0f
“jeonga-Son” <jjeong0106@gmail.com>
2023-03-31 10:40:57 +0900

    add CoffeeStatusRestController

---
+++ coffee-status/src/main/java/com/example/coffeestatus/controller/CoffeeStatusRestController.java
@@ -0,0 +1,12 @@
+package com.example.coffeestatus.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoffeeStatusRestController {
+    @GetMapping("/status")
+    public String hello() {
+        return "Coffee status";
+    }
+}
