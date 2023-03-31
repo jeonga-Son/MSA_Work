828b5b1a13dce959aa35d0a406a7924f7a4bcea6
“jeonga-Son” <jjeong0106@gmail.com>
2023-03-31 14:54:00 +0900

    [feat] update CoffeeStatusRestController

--- coffee-status/src/main/java/com/example/coffeestatus/controller/CoffeeStatusRestController.java
+++ coffee-status/src/main/java/com/example/coffeestatus/controller/CoffeeStatusRestController.java
@@ -1,14 +1,40 @@
 package com.example.coffeestatus.controller;
 
+import com.example.coffeestatus.service.CoffeeStatusService;
+import com.example.coffeestatus.vo.CoffeeStatusVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.core.Response;
+import java.util.List;
+
 @RestController
 @RequestMapping("/coffee-status")
 public class CoffeeStatusRestController {
+    @Autowired
+    private CoffeeStatusService coffeeStatusService;
+
     @GetMapping("/")
     public String coffeeStatus() {
         return "Coffee status";
     }
+
+    @GetMapping("/createStatusTable")
+    public void createStatusTable() {
+        coffeeStatusService.createStatusTable();
+    }
+
+    @GetMapping("/coffeeOrderStatus")
+    // 주문이 다되면 해당 주문 상태가 status로 dispatch 된다.
+    // 따라서 insert 작업이 필요하지 않다.
+    public ResponseEntity<List<CoffeeStatusVO>> coffeeOrderStatus() {
+        List<CoffeeStatusVO> list = coffeeStatusService.selectCoffeeOrderStatus();
+
+        return new ResponseEntity<List<CoffeeStatusVO>>(list,HttpStatus.OK);
+    }
+
 }
